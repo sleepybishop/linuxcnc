dad0dbfa6a921f51e2f431fc72830bd4b3a8c3e6
joe <calderon.joe@gmail.com>
2015-10-02 20:55:52 -0700

    update image-to-gcode to work with fedora bundled python

--- src/emc/usr_intf/axis/scripts/image-to-gcode.py
+++ src/emc/usr_intf/axis/scripts/image-to-gcode.py
@@ -17,16 +17,16 @@
 ## jepler@unpy.net
 
 import sys, os
-BASE = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), ".."))
+BASE = "/usr"
 sys.path.insert(0, os.path.join(BASE, "lib", "python"))
 
 import gettext;
 gettext.install("linuxcnc", localedir=os.path.join(BASE, "share", "locale"), unicode=True)
 
-import Image
+from PIL import Image
 
 try:
-    import numpy.numarray as numarray
+    import numpy as numarray
     import numpy.core
     olderr = numpy.core.seterr(divide='ignore')
     plus_inf = (numarray.array((1.,))/0.)[0]
@@ -63,7 +63,7 @@ def make_tool_shape(f, wdia, resp):
     dia = int(wdia*res+.5)
     wrad = wdia/2.
     if dia < 2: dia = 2
-    n = numarray.array([[plus_inf] * dia] * dia, type="Float32")
+    n = numarray.array([[plus_inf] * dia] * dia, dtype="float32")
     hdia = dia / 2.
     l = []
     for x in range(dia):
@@ -275,9 +275,9 @@ class Converter:
             tw, th = rough.shape
             w1 = w + tw
             h1 = h + th
-            nim1 = numarray.zeros((w1, h1), 'Float32') + base_image.min()
+            nim1 = numarray.zeros((w1, h1), dtype='float32') + base_image.min()
             nim1[tw/2:tw/2+w, th/2:th/2+h] = base_image
-            self.image = numarray.zeros((w,h), type="Float32")
+            self.image = numarray.zeros((w,h), dtype="float32")
             for j in range(0, w):
                 progress(j,w)
                 for i in range(0, h):
@@ -495,7 +495,7 @@ class ArcEntryCut:
 
 def ui(im, nim, im_name):
     import Tkinter
-    import ImageTk
+    from PIL import ImageTk
     import pickle
     import nf
 
@@ -760,7 +760,8 @@ def main():
     im = im.convert("L") #grayscale
     w, h = im.size
 
-    nim = numarray.fromstring(im.tostring(), 'UInt8', (h, w)).astype('Float32')
+    nim = numarray.fromstring(im.tostring(), 'uint8', h*w).astype('float32')
+    nim.shape = [h,w]
     options = ui(im, nim, im_name)
 
     step = options['pixelstep']
@@ -786,7 +787,7 @@ def main():
         tw, th = tool.shape
         w1 = w + 2*tw
         h1 = h + 2*th
-        nim1 = numarray.zeros((w1, h1), 'Float32') + pixel
+        nim1 = numarray.zeros((w1, h1), dtype='float32') + pixel
         nim1[tw:tw+w, th:th+h] = nim
         nim = nim1
         w, h = w1, h1
