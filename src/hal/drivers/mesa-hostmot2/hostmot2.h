186a83aeb9545f12d1e152750db612c3162e018b
joseph calderon <calderon.joe@gmail.com>
2017-12-23 13:56:39 -0800

    sigh

--- src/hal/drivers/mesa-hostmot2/hostmot2.h
+++ src/hal/drivers/mesa-hostmot2/hostmot2.h
@@ -103,8 +103,8 @@
 #define HM2_GTAG_SSI               (8)
 #define HM2_GTAG_UART_TX           (9)
 #define HM2_GTAG_UART_RX           (10)
-#define HM2_GTAG_PKTUART_TX        (28)  // PktUART uses same addresses as normal UART with 
-#define HM2_GTAG_PKTUART_RX        (29) // the assumption you would not use both in one config
+#define HM2_GTAG_PKTUART_TX        (27)  // PktUART uses same addresses as normal UART with 
+#define HM2_GTAG_PKTUART_RX        (28) // the assumption you would not use both in one config
 #define HM2_GTAG_TRANSLATIONRAM    (11)
 #define HM2_GTAG_MUXED_ENCODER     (12)
 #define HM2_GTAG_MUXED_ENCODER_SEL (13)
